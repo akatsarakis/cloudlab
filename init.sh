8aaf00ffb2132c1d09edeead088fab4d99474bda
Antonis Katsarakis <antoniskatsarakis@yahoo.com>
2021-03-09 18:57:24 +0200

    final changes?

--- init.sh
+++ init.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
+# WARNING: Before executing this script make sure to have setup
+#   ssh-key on github and cloudlab and have share it with executing node
 
-# Before executing this script make sure to have setup
-#   ssh-key on github and share it with executing node
-
+# TODO: Set the two variabled below
 GITHUB_USERNAME="akatsarakis"
 NO_NODES="5" # WARNING: cannot be higher than number of allocated nodes in cloudlab
-#OFED="MLNX_OFED_LINUX-5.2-2.2.0.0-ubuntu18.04-x86_64" # supports CX4 +
-OFED="MLNX_OFED_LINUX-5.0-2.1.8.0-ubuntu18.04-x86_64" # supports CX3 +
 
-# TODO return error if NO_NODES > 9
+# last OFED supporting CX3 nic
+OFED="MLNX_OFED_LINUX-5.0-2.1.8.0-ubuntu18.04-x86_64"
+
+if [[ "${NO_NODES}" -gt 9 ]]; then
+  echo "Current script supports up to 9 nodes"
+  exit 1;
+fi
 
 # [Optionally] set terminal bar --> must source ~/.bashrc to apply it
 echo " " >> ~/.bashrc
@@ -35,6 +39,7 @@ echo 10000000001 | sudo tee /proc/sys/kernel/shmmax
 echo 10000000001 | sudo tee /proc/sys/kernel/shmall
 
 ssh-keyscan -H github.com >> ~/.ssh/known_hosts
+git clone https://github.com/ease-lab/Hermes hermes
 git clone git@github.com:${GITHUB_USERNAME}/cloudlab.git # assumes ssh-key on github exists on the node
 cd cloudlab
 ./merge-chuncks.sh
@@ -47,46 +52,38 @@ sudo ./mlnxofedinstall --force; sleep 2
 # restart driver
 sudo /etc/init.d/openibd restart; sleep 2
 
-sudo mst start; sleep 2
+sudo mst start
 
-# Get node name
-# WARNING this works only for up to single-digit nodes
-# (i.e., up to node 9 but node node 10)
+sleep 20 # if we try to init nic immediately it typically fails
+
+# Setting the ip to the ib0 might not work on the first try so repeat
 MAX_RETRIES=10
 for i in `seq 1 ${MAX_RETRIES}`; do
   sudo ifconfig ib0 10.0.3.${HOSTNAME:5:1}/24 up
-  # it might not work immediatelly
-  if ibdev2netdev | grep "up" ; then
+  if ibdev2netdev | grep "Up" ; then
     break
   fi
-  sleep 2
+  sleep 5
 done
 
-# [Optionally] ensure everything was configured properly
-if ibdev2netdev | grep "up" ; then
-  echo "IB0 is UP!"
+if ibdev2netdev | grep "Up" ; then
+  echo "IB0 is Up!"
 else
   ibdev2netdev
-  echo "IB0 is not up --> setup failed!"
+  echo "IB0 is not Up --> setup failed!"
   exit 1
 fi
+
+# [Optionally] For dbg ensure everything was configured properly
 #ibdev2netdev # --> must show ib0 (up)
 #ifconfig --> expected ib0 w/ expected ip
 #ibv_devinfo --> PORT_ACTIVE
-
-#~~~~~~~~~
-#~~~~~~~~~  Hermes
-#~~~~~~~~~
-cd ~
-git clone https://github.com/ease-lab/Hermes hermes
-
-# [Optionally] follow instructions of Hermes' artifact
 
 #############################
 # WARNING only on first node!
 #############################
 if [[ "${HOSTNAME:5:1}" == 1 ]]; then
-    sleep 100 # give some time so that everyone has finished setup
+    sleep 100 # give some time so that all peers has setup their NICs
 
     # start a subnet manager
     sudo /etc/init.d/opensmd start # there must be at least one subnet-manager in an infiniband subnet cluster
